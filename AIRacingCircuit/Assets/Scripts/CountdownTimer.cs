73b5bacc4670b01e850993013b397ba12ca8fec8
dubz-skrrrt <chill.99.lr@gmail.com>
2021-02-17 10:51:41 +0800

    revert back to main

--- AIRacingCircuit/Assets/Scripts/CountdownTimer.cs
+++ AIRacingCircuit/Assets/Scripts/CountdownTimer.cs
@@ -10,27 +10,30 @@ public class CountdownTimer : MonoBehaviour
     float startingTime = 3f;
     
     public GameObject racetimer;
+    public GameObject leaderboard;
    
     [SerializeField] Text CountdownText;
 
     // Start is called before the first frame update
     void Start()
     {
-      
         currentTime = startingTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentTime -= 1 * Time.unscaledDeltaTime;
-        CountdownText.text  = currentTime.ToString("0");
+        if(currentTime > 1){
+            currentTime -= 1 * Time.unscaledDeltaTime;
+            CountdownText.text  = currentTime.ToString("0");
+        }
 
-        if(currentTime <= 1){
+        if(currentTime <= 1 && Time.timeScale == 0){
             currentTime = 1;
             CountdownText.enabled = false;
             Time.timeScale = 1;
             racetimer.SetActive(true);
+            leaderboard.SetActive(true);
             //  CountdownText.text  = currentTime.ToString("Go!");  
             // DisableText();        
         }
